5badc529446d5e9b90ce45c602033c8ba1f5d295
Rafael Ortiz <rafael.ortiz.barrera@gmail.com>
2020-04-03 11:23:41 -0300

    Se crea componente ListadoProducto

---
+++ src/producto/componentes/ListadoProductos.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Producto from './Producto'
+
+const ListadoProductos = () => {
+
+    return(
+        <Producto />
+    );
+
+
+}
+
+export default ListadoProductos;
