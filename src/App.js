c5227db00361602412bed8e90df367f850d00378
Rafael Ortiz <rafael.ortiz.barrera@gmail.com>
2020-04-03 11:27:05 -0300

    gitflow-feature-stash: unir-header-listado-producto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './header/Header'
+import ListadoProducto from './producto/componentes/ListadoProductos'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render() {    
+        return(
+            <Fragment>
+                <Header />
+                <br />
+                <Container>
+                    <Row>
+                        <Col></Col>
+                        <Col xs={10}><ListadoProducto /></Col>
+                        <Col></Col>
+                    </Row>
+                </Container>
+            </Fragment>
+        );
+    }
 }
 
 export default App;
