6887d7ccc5aca1d1fba129bf75e86719ce60fde8
Rafael Ortiz <rafael.ortiz.barrera@gmail.com>
2020-04-05 19:54:46 -0400

    gitflow-feature-stash: rutas-pagina-web

--- src/header/Header.js
+++ src/header/Header.js
@@ -5,26 +5,30 @@ import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
 import FormControl from 'react-bootstrap/FormControl';
 import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom';
 import './Header.css'
 
-const Header = () => {
-    return (
-        <>
-            <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="#home"><img src={logo} height="80" /></Navbar.Brand>
-                <Nav className="mr-auto">
-                    <Nav.Link bsPrefix="nav-bike" href="#home" >Home</Nav.Link>
-                    <Nav.Link bsPrefix="nav-bike" href="#features">Features</Nav.Link>
-                    <Nav.Link bsPrefix="nav-bike" href="#pricing">Pricing</Nav.Link>
-                </Nav>
-                <Form inline>
-                    <FormControl type="text" placeholder="Buscar" className="mr-sm-2" />
-                    <Button variant="outline-light">Buscar</Button>
-                </Form>
-            </Navbar>
+class Header extends React.Component {
+    render() {
+        return (
+            <>
+                <Navbar bg="dark" variant="dark">
+                    <Navbar.Brand href="#home"><img src={logo} height="80" /></Navbar.Brand>
+                    <Nav className="mr-auto">
+                        <Nav.Link bsPrefix="nav-bike" href="/">Home</Nav.Link>
+                        <Nav.Link bsPrefix="nav-bike" href="/crearProducto">Crear Producto</Nav.Link>
+                        <Nav.Link bsPrefix="nav-bike" href="#pricing">Pricing</Nav.Link>
+                    </Nav>
 
-        </>
-    );
+                    <Form inline>
+                        <FormControl type="text" placeholder="Buscar" className="mr-sm-2" />
+                        <Button variant="outline-light">Buscar</Button>
+                    </Form>
+                </Navbar>
+
+            </>
+        );
+    }
 }
 
 export default Header;
