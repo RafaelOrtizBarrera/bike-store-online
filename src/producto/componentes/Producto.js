8afbdfdb32d1ae20e75a642db6dc131b96d61882
Rafael Ortiz <rafael.ortiz.barrera@gmail.com>
2020-05-06 12:14:22 -0400

    propiedades de imagen
    se agrega url de aws bucket. se soluciona problema con estilos

--- src/producto/componentes/Producto.js
+++ src/producto/componentes/Producto.js
@@ -6,8 +6,8 @@ const Producto = (props) => {
 
     return (
         <>
-            <Card style={{ width: '18rem', float: 'left', 'margin-right': '15px',  'margin-top': '10px', 'margin-bottom': '10px'}}>
-                <Card.Img variant="top" src="holder.js/100px180" />
+            <Card style={{ width: '18rem', float: 'left', 'marginRight': '15px',  'marginTop': '10px', 'marginBottom': '10px'}}>
+                <Card.Img variant="top" src={props.producto.urlImg} />
                 <Card.Body>
                     <Card.Title>{props.producto.modelo}</Card.Title>
                     <Card.Text>
